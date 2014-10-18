aa7b23c99f12f90368f00bd0479638a66942fefc
gilad1987 <gilad19872@gmail.com>
2014-10-18 21:28:24 +0300

    html5 audio directive

--- src/js/html5player/html5player.ctrl.js
+++ src/js/html5player/html5player.ctrl.js
@@ -4,13 +4,13 @@
     {
         this.player = {
             path : '../files/audio/0bf2d6de463070a01842acaa7a8f13f8_9424.mp3'
-        }
+        };
 
 
 
         this.changePath = function(){
             this.player.path = '../files/audio/3a261737929931198c3219f941f5152e_1763.mp3'
-        }
+        };
     }
 
     angular.module('html5player').controller('Html5PlayerController',['$scope','PLAYER_EVENTS', Html5PlayerController]);
